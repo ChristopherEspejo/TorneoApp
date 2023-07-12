6d6a103974b8b0b26ad12ea1daaf7fefcc7d81d1
ChristopherEspejo <32018635+ChristopherEspejo@users.noreply.github.com>
2023-07-11 21:15:20 -0500

    fix team model

--- src/models/Team.js
+++ src/models/Team.js
@@ -1,27 +1,28 @@
 const mongoose = require('mongoose');
 
 const TeamSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    creator: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    members: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }],
-    created_at: {
-        type: Date,
-        default: Date.now
-    },
-    updated_at: {
-        type: Date,
-        default: Date.now
-    }
+  name: {
+    type: String,
+    required: true,
+    index: false // Remover el índice único en el campo 'name'
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  members: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model('Team', TeamSchema);
