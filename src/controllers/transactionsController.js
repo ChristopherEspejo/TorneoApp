0e4c1849bcc676457f3f9b5e2c7bc4a2866c4a38
Kevin espejo <32018635+ChristopherEspejo@users.noreply.github.com>
2024-08-04 22:55:18 -0500

    fix: completar trasnaccion sin correo

--- src/controllers/transactionsController.js
+++ src/controllers/transactionsController.js
@@ -223,83 +223,82 @@ exports.completeTransaction = async (req, res) => {
     const { comentario, imagen } = req.body; // Información proporcionada en la solicitud
     const transactionId = req.params.id;
     const transaction = await Transaction.findById(transactionId);
-    if(userRole.rol !== 'admin'){
+    if (userRole.rol !== 'admin') {
       return res.status(403).json({ message: 'No tienes permiso para realizar esta acción' });
     }
     if (!transaction) {
       return res.status(404).send('Transacción no encontrada');
     }
-
     if (transaction.estado === 'culminado' || transaction.estado === 'cancelado') {
       return res.status(400).send('La transacción no puede modificarse en su estado actual');
     }
-
     if (!imagen) {
       return res.status(400).send('Se requiere una imagen para completar la transacción');
     }
-
-    // Buscar información del usuario para enviar el correo
     const user = await User.findById(transaction.usuarioId);
     if (!user) {
       return res.status(404).send('Usuario no encontrado');
     }
 
-    // Preparar el correo electrónico de confirmación
-    const htmlContent = `
-    <html>
-    <head>
-      <style>
-        body { font-family: Arial, sans-serif; line-height: 1.6; }
-        .header { color: #2E5984; font-size: 20px; font-weight: bold; }
-        .info { background-color: #f2f2f2; padding: 10px; border-radius: 5px; }
-        .footer { margin-top: 20px; font-size: 16px; color: #444; }
-      </style>
-    </head>
-    <body>
-      <div class="header">Confirmación de Transacción</div>
-      <p>Estimado ${user.nombre},</p>
-      <p>Tu transacción ha sido completada exitosamente con los siguientes detalles:</p>
-      <div class="info">
-        <p><strong>Tipo de Operación:</strong> ${transaction.tipoOperacion === 'tipoCompra' ? 'Compra' : 'Venta'}</p>
-        <p><strong>Cantidad Enviada:</strong> ${transaction.cantidadEnvio} ${transaction.tipoOperacion === 'tipoCompra' ? 'PEN' : 'USD'}</p>
-        <p><strong>Cantidad Recibida:</strong> ${transaction.cantidadRecepcion} ${transaction.tipoOperacion === 'tipoCompra' ? 'USD' : 'PEN'}</p>
-        <p><strong>Banco Destino:</strong> ${transaction.bancoDestino}</p>
-        <p><strong>Número de Cuenta:</strong> ${transaction.numeroCuentaInterbancario}</p>
-      </div>
-      <div class="footer">
-        Gracias por usar nuestros servicios. Si tienes alguna pregunta, no dudes en contactarnos.
-      </div>
-    </body>
-    </html>
-    `;  // Tu HTML aquí
-
-    // Intentar enviar el correo electrónico
-    // Intentar enviar el correo electrónico
-    const { data, error } = await resend.emails.send({
-      from: 'Cambialo <carlos.amorin@cambialo.com.pe>',  // Usar una dirección bajo tu dominio verificado
-      to: [user.email],  // Ahora puedes enviar a cualquier correo
-      subject: 'Confirmación de Transacción',
-      html: htmlContent
-    });
-
-
-    if (error) {
-      console.error(error);
-      return res.status(500).send('Error al enviar el correo electrónico');
-    }
-
-    // Actualizar y guardar la transacción solo si el correo fue enviado exitosamente
+    // Actualizar la transacción antes de intentar enviar el correo
     transaction.imagen = imagen;
     if (comentario) transaction.comentario = comentario;
     transaction.estado = 'culminado';
     await transaction.save();
 
-    res.json({ message: 'Transacción culminada y correo enviado con éxito', transaction, emailInfo: data });
+    if (user.email) {
+      const htmlContent = `
+      <html>
+      <head>
+        <style>
+          body { font-family: Arial, sans-serif; line-height: 1.6; }
+          .header { color: #2E5984; font-size: 20px; font-weight: bold; }
+          .info { background-color: #f2f2f2; padding: 10px; border-radius: 5px; }
+          .footer { margin-top: 20px; font-size: 16px; color: #444; }
+        </style>
+      </head>
+      <body>
+        <div class="header">Confirmación de Transacción</div>
+        <p>Estimado ${user.nombre},</p>
+        <p>Tu transacción ha sido completada exitosamente con los siguientes detalles:</p>
+        <div class="info">
+          <p><strong>Tipo de Operación:</strong> ${transaction.tipoOperacion === 'tipoCompra' ? 'Compra' : 'Venta'}</p>
+          <p><strong>Cantidad Enviada:</strong> ${transaction.cantidadEnvio} ${transaction.tipoOperacion === 'tipoCompra' ? 'PEN' : 'USD'}</p>
+          <p><strong>Cantidad Recibida:</strong> ${transaction.cantidadRecepcion} ${transaction.tipoOperacion === 'tipoCompra' ? 'USD' : 'PEN'}</p>
+          <p><strong>Banco Destino:</strong> ${transaction.bancoDestino}</p>
+          <p><strong>Número de Cuenta:</strong> ${transaction.numeroCuentaInterbancario}</p>
+        </div>
+        <div class="footer">
+          Gracias por usar nuestros servicios. Si tienes alguna pregunta, no dudes en contactarnos.
+        </div>
+      </body>
+      </html>
+      `;  // Tu HTML aquí
+
+      // Intentar enviar el correo electrónico
+      const { data, error } = await resend.emails.send({
+        from: 'Cambialo <carlos.amorin@cambialo.com.pe>',
+        to: [user.email],
+        subject: 'Confirmación de Transacción',
+        html: htmlContent
+      });
+
+      if (error) {
+        console.error(error);
+        return res.status(500).send('Error al enviar el correo electrónico');
+      }
+
+      res.json({ message: 'Transacción culminada exitosamente', transaction, emailInfo: data });
+    } else {
+      // Si no hay correo, solo confirmar la culminación de la transacción
+      res.json({ message: 'Transacción culminada exitosamente', transaction });
+    }
   } catch (error) {
     console.error(error);
     res.status(500).send('Error al actualizar el estado de la transacción');
   }
 };
+
 
 // Asegúrate de implementar esta función según tu lógica de negocio para calcular la cantidad de recepción
 function calcularCantidadRecepcion(cantidadEnvio, tipoCambio,tipoOperacion) {
